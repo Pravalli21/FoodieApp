a28de71d7b7b27a21f509377c4441e1e64e09031
Guru Pravallika <71175027+pravallika-pro@users.noreply.github.com>
2024-09-26 10:20:56 +0530

    Foodie project

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const dotenv=require("dotenv");
+dotenv.config();
+const express=require("express");
+const mongoose=require("mongoose");
+const app=express();
+
+const cors=require("cors");
+app.use(cors());
+const cookieparser=require("cookie-parser")
+app.use(cookieparser());
+const router=require('./serverroutes/Serverroutes');
+app.use(router);
+const bodyparser=require("body-parser");
+app.use(bodyparser.urlencoded({extended:true,parameterLimit:100000,limit:"500mb"}));
+// app.use(express.json());
+app.use(bodyparser.json());
+app.use('/images', express.static('images'));
+
+//mongoose connection
+mongoose.connect(process.env.dbconnect,({useNewUrlParser:true,useUnifiedTopology: true})).then(()=>{
+    console.log("connectedtodb");
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server running");
+})
---
+++ server/models/Signup.js
@@ -0,0 +1,57 @@
+const dotenv=require("dotenv");
+dotenv.config();
+const mongoose=require("mongoose");
+const validator=require("validator");
+const jwt=require("jsonwebtoken");
+
+const Myschema=new mongoose.Schema({
+    
+    Gmail:{
+        type:"String",
+        required:[true,"Gmail is required"],
+        validate:[validator.isEmail,"gmail is invalid"],
+        lowercase:true
+    },
+    Password:{
+        type:"String",
+        min:[6,"Min length has to be 6"],
+        required:[true,"Password is required"],
+    },
+    Date:{
+        type:"Date",
+        default:Date()
+    },
+    tokens:[
+        {
+            passkey:{
+                type:"String"
+            }
+        }
+    ],
+    user:{
+        type:Boolean,
+        default:false
+    }
+});
+
+Myschema.methods.generatelogtoken=async function(){
+    try{
+        let token=await jwt.sign({_id:this._id},process.env.jwttoken);
+        if(this.tokens){
+            this.tokens.pop();
+        }
+        this.tokens=await this.tokens.concat({passkey:token});
+        await this.save();
+        return token;
+
+
+    }catch(err){
+        console.log("pravallika pravallika")
+        console.log(err);
+
+    }
+}
+
+const Register_user= mongoose.model("Registered_users",Myschema);
+
+module.exports=Register_user;
